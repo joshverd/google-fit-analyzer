ff50cc0ff87a7de5c7c32bb1184f9af44b4a9378
BuildTools <joshverd2@gmail.com>
2020-08-30 15:09:49 -0500

    Documented code and added support for workout arrays parsed out of the XML data

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // https://stackoverflow.com/questions/40593875/using-filesystem-in-node-js-with-async-await
+//
+// Module Imports
 const { promises: fs } = require('fs');
 const xmlParser = require('fast-xml-parser');
 const { plot, stack, clear, Plot } = require('nodeplotlib');
@@ -11,7 +13,15 @@ const moment = require('moment');
 //    BAD : '/data'
 const dataDirectory = './data/';
 
-// Top level await PLSSS
+// Which workouts should we analyze?
+//
+// NOTE: To analyze all workouts, make this '*'
+//
+// If you want to find which type a workout is, check the .TXC file for the <Notes></Notes> XML field.
+// Some examples are: 'Biking', 'Walking'
+const workoutTypeToAnalyze = 'Biking';
+
+// I really need to update to node v14.3 for top-level await...
 (async () => {
   // Pull all the items from the data directory
   const files = await fs.readdir(dataDirectory);
@@ -23,7 +33,10 @@ const dataDirectory = './data/';
 
   // Loop through all the file names
   for(const fileName of files) {
-    // Read file
+    // Ignore the .gitignore file, since it's not the file type that we need!
+    if(fileName === '.gitignore') continue;
+
+    // Read the file
     let data = await fs.readFile(`${dataDirectory}${fileName}`);
 
     // Turn the raw file data into a string so it can be parsed as XML
@@ -34,22 +47,40 @@ const dataDirectory = './data/';
 
     if(!parsedXML) return console.warn('Found unparsed XML file', fileName, parsedXML);
 
-    // The workout data pulled from the parsed XML
-    const workoutData = parsedXML.TrainingCenterDatabase.Activities.Activity;
+    // The workouts data pulled from the parsed XML
+    //
+    // This data can be one of the following:
+    //  - WorkoutObject
+    //  - WorkoutObject[]
+    let parsedWorkoutsData = parsedXML.TrainingCenterDatabase.Activities.Activity;
 
-    if(workoutData.Notes !== 'Biking') {
-      console.log('This workout is not a biking workout');
+    // If the parsed workout Data is not an array (just a normal WorkoutObject), turn it into a single object array
+    if(!Array.isArray(parsedWorkoutsData)) parsedWorkoutsData = [ parsedWorkoutsData ];
 
-      continue;
+    // Loop through the parsedWorkoutsData array and analyze each workout individually
+    for(const workoutData of parsedWorkoutsData) {
+      // What workout type is this?
+      const workoutType = workoutData.Notes;
+
+      // Is the workout type the same type that we are targeting?
+      // If the workout type to analyze is a '*', that means we want all workouts
+      if(workoutTypeToAnalyze !== '*' && workoutType !== workoutTypeToAnalyze) {
+        console.log(`This workout is not a ${workoutTypeToAnalyze} workout, it is a ${workoutType} workout.`);
+
+        continue;
+      };
+
+      // Log the workout object for easier debugging
+      // console.log(workoutData);
+
+      // Push the workout to the workouts array
+      workouts.push(workoutData);
     };
-
-    console.log(workoutData);
-
-    // Push the workout to the workouts array
-    workouts.push(workoutData);
   };
 
-  console.log(workouts.length);
+  if(workouts.length === 0) return console.error('Unable to find any workouts to analyze.');
+
+  console.log(`Found ${workouts.length} workouts to analyze!`);
 
   // X and Y axis for the chart
   let chartDataX = [];
@@ -64,29 +95,28 @@ const dataDirectory = './data/';
     // recording random workouts during the day for no reason and causes the graph to look weird
     if(workout.TotalTimeSeconds <= 100) continue;
 
-    // Parse the time when this workout happened
-    const time = moment.utc(workout.Id).format('LLL');
+    // Parse the date of when this workout happened
+    const date = moment.utc(workout.Id).format('LLL');
 
-    // if(!workout.Lap.AverageHeartRateBpm) continue;
-    // if(!workout.Lap.AverageHeartRateBpm) continue;
-
-    // This gets plotted on the Y axis.
+    // This 'valueOfInterest' gets plotted on the Y axis.
+    //
+    // 1 meter to 1 mile === .000621371
     const valueOfInterest = workout.Lap.DistanceMeters * 0.000621371;
     // const valueOfInterest = (workout.Lap.AverageHeartRateBpm.Value + workout.Lap.AverageHeartRateBpm.Value) / 2;
 
-    if(valueOfInterest <= .05) continue;
-
+    // Add the value of interest to the Y axis total
     totalYAxis += valueOfInterest;
 
-    // Push the time to the X axis
-    chartDataX.push(time);
+    // Push the date to the X axis
+    chartDataX.push(date);
 
-    // Push this variable to the Y axis
+    // Push the value of interest to the Y axis data
     chartDataY.push(valueOfInterest);
   };
 
   console.log(`Parsed ${workouts.length} workouts, all Y-Axis values added together equal ${totalYAxis}, averaging to ${(totalYAxis / workouts.length).toFixed(2)} per workout.`);
 
+  // Chart data configuration. We want a line graph with x and y data.
   const chartData = [
     {
       x: chartDataX,
